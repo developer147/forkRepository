b61b178e833c8f1e7e38e6a8f6ce304850cfe557
Jay Harkar <jayshekar@gmail.com>
2023-03-09 20:24:32 -0600

    Removed printlns and redirect to an URI on authorization

--- app.py
+++ app.py
@@ -8,33 +8,13 @@ app = Flask(__name__)
 Bootstrap(app)
 environment_configuration = os.environ['CONFIGURATION_SETUP']
 app.config.from_object(environment_configuration)
-print(f"Environment: {app.config['ENV']}")
-print(f"Debug: {app.config['DEBUG']}")
-
-# github_app_id = app.config['GITHUB_APP_ID']
-# print(f"Github App ID: {github_app_id}")
-
 github_app_client_id = app.config['GITHUB_APP_CLIENT_ID']
-print(f"Github App Client ID: {github_app_client_id}")
-
 redirect_uri_on_authorization = app.config['REDIRECT_URI_ON_AUTHORIZATION']
-print(f"Redirect Uri On Authorization: {redirect_uri_on_authorization}")
-
 redirect_state = app.config['REDIRECT_STATE']
-print(f"Redirect State: {redirect_state}")
-
 github_app_client_secret = app.config['GITHUB_APP_CLIENT_SECRET']
-print(f"Github App Client Secret: {github_app_client_secret}")
-
 redirect_uri_for_access_token = app.config['REDIRECT_URI_FOR_ACCESS_TOKEN']
-print(f"Redirect Uri For Access Token: {redirect_uri_for_access_token}")
-
 github_fork_repo_app_uri = app.config['GITHUB_FORK_REPO_APP_URI']
-print(f"Github Fork Repo App URI: {github_fork_repo_app_uri}")
-
 authorization_uri = app.config['AUTHORIZATION_URI']
-print(f"Authorization URI: {authorization_uri}")
-
 
 @app.route('/')
 def index():
@@ -57,17 +37,12 @@ def authorize():
 def fork_repo():
     code = request.args.get('code')
     state = request.args.get('state')
-    print('code', code, 'state', state)
 
     dictToSend = {'client_id': github_app_client_id, 'client_secret': github_app_client_secret,
                   'code': code, 'redirect_uri': redirect_uri_for_access_token}
     headers = {"Accept":"application/json"}
     response = requests.post('https://github.com/login/oauth/access_token', json=dictToSend, headers=headers)
-    print("status code", response.status_code)
     json_res = response.json()
-    print(response.text)
-    #json_data = json.loads(response.text)
-    print("access_token", json_res['access_token'], "expires_in", json_res['expires_in'], "refresh_token", json_res['refresh_token'], "refresh_token_expires_in", json_res["refresh_token_expires_in"], "token_type", json_res["token_type"], "scope", json_res["scope"])
 
     # fork repository code
     # https://docs.github.com/en/rest/repos/forks?apiVersion=2022-11-28#create-a-fork
@@ -83,14 +58,12 @@ def fork_repo():
     json_res = json.loads(response.text)
 
     html_url = json_res["html_url"]
-    print("HTML URL", json_res["html_url"])
 
     return redirect(html_url)
 
 @app.route('/accessToken')
 def access_token():
     access_token = request.args.get('access_token')
-    print('access_token', access_token)
     return 'Ok'
 
 if __name__ == "__main__":
